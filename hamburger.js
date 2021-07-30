b32f29aa07c3ed095a2fcea75968222717ea7db4
ksenia-nesytova <alleheldentot@gmail.com>
2021-07-30 07:02:01 +0700

    added border to form

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,14 @@
 const navToggle = document.getElementById('nav-toggle');
 
+const nav = document.getElementsByClassName('nav');
+
 navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+  nav.classList.add('nav-open');
+  alert("working")
+});
+
+
+navToggle.addEventListener('click', () => {
+  nav.classList.remove('nav-open');
   alert("working")
 });
